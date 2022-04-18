817cc3b38877510cb999f0d85daeabeeb43a2147
necator9 <Necator94@gmail.com>
2022-04-18 19:53:36 +0200

    Add remaining out csv parameters

--- saver.cpp
+++ saver.cpp
@@ -12,7 +12,7 @@ Saver::Saver(const std::string out_dir_, const int *counter_):
     fs::path file("output.csv");
     fs::path full_csv_path = dir / file;
     mycsvfile.open(full_csv_path);
-    mycsvfile << "img,o_num,rw_w,rw_h,rw_ca,rw_z,rw_x,ca,x,y,w,h,       o_prob,o_class,av_bin,lamp" << std::endl;
+    mycsvfile << "img,o_num,rw_w,rw_h,rw_ca,rw_z,rw_x,ca,x,y,w,h,o_prob,o_class,av_bin" << std::endl;
 };
 
 void Saver::close() {
@@ -24,8 +24,7 @@ void Saver::save_csv(const Frame &fr) {
         mycsvfile << *counter << "," << i << "," << fr.basic_params[i].rw_w << "," << fr.basic_params[i].rw_h << "," << 
         fr.basic_params[i].rw_ca << "," << fr.basic_params[i].rw_d << "," << fr.basic_params[i].rw_xc << "," <<   
         fr.basic_params[i].ca << "," << fr.basic_params[i].rect.x << "," << fr.basic_params[i].rect.y << "," << 
-        fr.basic_params[i].rect.width << "," << fr.basic_params[i].rect.height << "," 
-
-        << std::endl;
+        fr.basic_params[i].rect.width << "," << fr.basic_params[i].rect.height << "," << fr.prob[i] << "," <<
+        fr.o_class[i] << std::endl;
     }
 }
